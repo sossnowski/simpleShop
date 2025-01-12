ced99b2323974b7d8c9c25fb8c89cc5548daad66
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2025-01-12 14:17:01 +0100

    error handling added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import http from 'http';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import { connection } from 'config/db';
+import { errorHandler, handleUnhandledExceptions } from 'middlewares/errorHandler';
+import { errorFactory } from 'utils/errors/errorFactory';
 
 dotenv.config();
 
@@ -11,6 +16,17 @@ const port = process.env.PORT;
 
 let server: http.Server;
 let dbClient: Sequelize | undefined;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use((
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => next(errorFactory().notFoundError(req.path)));
+app.use(errorHandler);
 
 const startServer = async () => {
   try {
@@ -27,6 +43,7 @@ const startServer = async () => {
 };
 
 startServer();
+handleUnhandledExceptions();
 
 process.on('SIGTERM', () => {
   // eslint-disable-next-line no-console
